3f06efd3dfcad1dea7d349d54078e73716d0ad97
Mike Jankulak <131381838+mikejankulaknoaa@users.noreply.github.com>
2023-05-31 13:29:27 -0400

    adds separate Clock module and tests it

---
+++ Erl2Clock.py
@@ -0,0 +1,65 @@
+#! /usr/bin/python3
+
+from tkinter import *
+from tkinter import ttk
+from time import strftime
+
+# clock/calendar widget adapted from an example found at
+# https://www.geeksforgeeks.org/python-create-a-digital-clock-using-tkinter/#
+
+class Erl2Clock():
+
+    def __init__(self, parent, clockType='oneline', withSeconds=False):
+        self.__parent = parent
+        self.__clockType = clockType
+        self.__withSeconds = withSeconds
+
+        self.__frame = ttk.Frame(self.__parent, padding='2 2', relief='solid', borderwidth=1)
+        self.__frame.grid(column=1, row=1, padx='2', pady='2', sticky='ne')
+
+        # add the subframes for time and date
+        self.__clockTime = ttk.Label(self.__frame, text='14:20', font='Arial 16 bold'
+            #, relief='solid', borderwidth=1
+            )
+        self.__clockDate = ttk.Label(self.__frame, text='1.16.23', font='Arial 12'
+            #, relief='solid', borderwidth=1
+            )
+
+        # different positioning depending on clock type
+        if self.__clockType == 'oneline':
+            self.__clockTime.grid(column=1, row=1, sticky='s')
+            self.__clockDate.grid(column=2, row=1, sticky='s')
+            self.__frame.columnconfigure(1,weight=1)
+            self.__frame.columnconfigure(2,weight=1)
+            self.__frame.rowconfigure(1,weight=1)
+        else:
+            self.__clockTime.grid(column=1, row=1, sticky='n')
+            self.__clockDate.grid(column=1, row=2, sticky='s')
+            self.__frame.columnconfigure(1,weight=1)
+            self.__frame.rowconfigure(1,weight=1)
+            self.__frame.rowconfigure(2,weight=1)
+
+        # start the clock running
+        self.myClock()
+
+    # this method runs every second to update the clock readout
+    def myClock(self):
+        # I am zero-padding the hour, minute and day-of-month, but not the month
+        if self.__withSeconds:
+            clockTime = strftime('%H:%M:%S')
+        else:
+            clockTime = strftime('%H:%M')
+        clockDate = strftime('%-m.%d.%y')
+        self.__clockTime.config(text=clockTime)
+        self.__clockDate.config(text=clockDate)
+        self.__clockDate.after(1000, self.myClock)
+
+def main():
+
+    root = Tk()
+    clock = Erl2Clock(root,clockType='oneline',withSeconds=True)
+    #clock.myClock()
+    root.mainloop()
+
+if __name__ == "__main__": main()
+
---
+++ Erl2Tank.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3
+
+from tkinter import *
+from tkinter import ttk
+from Erl2Clock import Erl2Clock
+
+class Erl2Tank:
+
+    def __init__(self, parent):
+        self.__parent = parent
+
+        # stylistic stuff
+        s = ttk.Style()
+        s.configure('TNotebook',tabposition='n',borderwidth=0,padding=0)
+        s.configure('TNotebook.Tab',font='Arial 16 italic',padding=4)
+
+        # the top-level element is a notebook (tabbed screens)
+        mainTabs = ttk.Notebook(self.__parent, padding='0')
+        mainTabs.grid(column=1, row=1, pady='0', sticky='nwse')
+        mainTabs.columnconfigure(1, weight=1)
+        mainTabs.rowconfigure(1, weight=1)
+
+        dataTab = ttk.Frame(mainTabs, padding='0')
+        dataTab.grid_columnconfigure(1,weight=1)
+        dataTab.grid_rowconfigure(1,weight=1)
+        mainTabs.add(dataTab,text='Data',padding=0)
+
+        tempTab = ttk.Frame(mainTabs)
+        tempTab.grid_columnconfigure(1,weight=1)
+        tempTab.grid_rowconfigure(1,weight=1)
+        mainTabs.add(tempTab,text='Temp',padding=0)
+
+        phTab = ttk.Frame(mainTabs)
+        phTab.grid_columnconfigure(1,weight=1)
+        phTab.grid_rowconfigure(1,weight=1)
+        mainTabs.add(phTab,text='pH',padding=0)
+
+        o2Tab = ttk.Frame(mainTabs)
+        o2Tab.grid_columnconfigure(1,weight=1)
+        o2Tab.grid_rowconfigure(1,weight=1)
+        mainTabs.add(o2Tab,text='DO',padding=0)
+
+        settingsTab = ttk.Frame(mainTabs)
+        settingsTab.grid_columnconfigure(1,weight=1)
+        settingsTab.grid_rowconfigure(1,weight=1)
+        mainTabs.add(settingsTab,text='Settings',padding=0)
+
+        # add a clock widget
+        clock = Erl2Clock(self.__parent,withSeconds=True)
+
+def main():
+
+    root = Tk()
+    tank = Erl2Tank(root)
+    root.mainloop()
+
+if __name__ == "__main__": main()
