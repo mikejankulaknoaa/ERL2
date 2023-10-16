bf144333bdeef8e602009ab06e236a81286f27d6
mikejankulaknoaa <mike.jankulak@noaa.gov>
2023-10-16 17:02:30 -0400

    fixes startup bug re: logging frequencies

--- python/Erl2Config.py
+++ python/Erl2Config.py
@@ -13,7 +13,7 @@ from tzlocal import get_localzone
 class Erl2Config():
 
     # hardcoded ERL2 version string
-    VERSION = '0.22b (2023-10-11)'
+    VERSION = '0.23b (2023-10-16)'
 
     # top-level categories in the erl2.conf file
     CATEGORIES = [ 'system', 'device', 'virtualtemp', 'temperature', 'pH', 'DO', 'generic', 'heater', 'chiller', 'mfc.air', 'mfc.co2', 'mfc.n2']
